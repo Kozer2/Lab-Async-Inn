d4d5d9c1e29a8e761db66546ffe53dc89fa833b1
Kozer2 <benhemann@yahoo.com>
2021-04-18 13:32:19 -0500

    added signing key

--- Async-Inn/Async-Inn/Services/JwtTokenService.cs
+++ Async-Inn/Async-Inn/Services/JwtTokenService.cs
@@ -1,9 +1,11 @@
 ﻿using Async_Inn.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Async_Inn.Services
@@ -21,7 +23,26 @@ namespace Async_Inn.Services
 
         public async Task<string> GetToken(ApplicationUser user, TimeSpan expiresIn)
         {
+            var principal = await signInManager.CreateUserPrincipalAsync(user);
+            if (principal == null)
+                return null;
+
+
+            var signingKey = GetSecurityKey(configuration);
             return "token!"; 
         }
+            
+        private static SecurityKey GetSecurityKey(IConfiguration configuration)
+        {
+            var secret = configuration["JWT:Secret"];
+
+            if (secret == null)
+                throw new InvalidOperationException("JWT:Secret is missing!");
+
+            var secretBytes = Encoding.UTF8.GetBytes(secret);
+            return new SymmetricSecurityKey(secretBytes);
+        }
+
+
     }
 }
